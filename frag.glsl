8dff2a66483dc77322a41118cf1eb1c58026464d
Xethro185 <91663538+Xethro185@users.noreply.github.com>
2021-09-30 11:50:56 +0200

    Update frag.glsl

--- frag.glsl
+++ frag.glsl
@@ -1,160 +1,22 @@
-#version 400 compatibility
-#extension GL_ARB_gpu_shader_fp64 : enable
-#pragma optionNV(fastmath off)
-#pragma optionNV(fastprecision off)
-
-#define FLOAT float
-#define VEC2 vec2
-#define VEC3 vec3
-#define AA_LEVEL 1
-#define ESCAPE 1000.0
-#define PI 3.141592653
-
-#define FLAG_DRAW_MSET ((iFlags & 0x01) == 0x01)
-#define FLAG_DRAW_JSET ((iFlags & 0x02) == 0x02)
-#define FLAG_USE_COLOR ((iFlags & 0x04) == 0x04)
-
-uniform vec2 iResolution;
-uniform vec2 iCam;
-uniform vec2 iJulia;
-uniform float iZoom;
-uniform int iType;
-uniform int iIters;
-uniform int iFlags;
-uniform int iTime;
-
-#define cx_one VEC2(1.0, 0.0)
-VEC2 cx_mul(VEC2 a, VEC2 b) {
-  return VEC2(a.x*b.x - a.y*b.y, a.x*b.y + a.y*b.x);
-}
-VEC2 cx_sqr(VEC2 a) {
-  FLOAT x2 = a.x*a.x;
-  FLOAT y2 = a.y*a.y;
-  FLOAT xy = a.x*a.y;
-  return VEC2(x2 - y2, xy + xy);
-}
-VEC2 cx_cube(VEC2 a) {
-  FLOAT x2 = a.x*a.x;
-  FLOAT y2 = a.y*a.y;
-  FLOAT d = x2 - y2;
-  return VEC2(a.x*(d - y2 - y2), a.y*(x2 + x2 + d));
-}
-VEC2 cx_div(VEC2 a, VEC2 b) {
-  FLOAT denom = 1.0 / (b.x*b.x + b.y*b.y);
-  return VEC2(a.x*b.x + a.y*b.y, a.y*b.x - a.x*b.y) * denom;
-}
-VEC2 cx_sin(VEC2 a) {
-  return VEC2(sin(a.x) * cosh(a.y), cos(a.x) * sinh(a.y));
-}
-VEC2 cx_cos(VEC2 a) {
-  return VEC2(cos(a.x) * cosh(a.y), -sin(a.x) * sinh(a.y));
-}
-VEC2 cx_exp(VEC2 a) {
-  return exp(a.x) * VEC2(cos(a.y), sin(a.y));
-}
-
-//Fractal equations
-VEC2 mandelbrot(VEC2 z, VEC2 c) {
-  return cx_sqr(z) + c;
-}
-VEC2 burning_ship(VEC2 z, VEC2 c) {
-  return VEC2(z.x*z.x - z.y*z.y, 2.0*abs(z.x * z.y)) + c;
-}
-VEC2 feather(VEC2 z, VEC2 c) {
-  return cx_div(cx_cube(z), cx_one + z*z) + c;
-}
-VEC2 sfx(VEC2 z, VEC2 c) {
-  return z * dot(z,z) - cx_mul(z, c*c);
-}
-VEC2 henon(VEC2 z, VEC2 c) {
-  return VEC2(1.0 - c.x*z.x*z.x + z.y, c.y * z.x);
-}
-VEC2 duffing(VEC2 z, VEC2 c) {
-  return VEC2(z.y, -c.y*z.x + c.x*z.y - z.y*z.y*z.y);
-}
-VEC2 ikeda(VEC2 z, VEC2 c) {
-  FLOAT t = 0.4 - 6.0/(1.0 + dot(z,z));
-  FLOAT st = sin(t);
-  FLOAT ct = cos(t);
-  return VEC2(1.0 + c.x*(z.x*ct - z.y*st), c.y*(z.x*st + z.y*ct));
-}
-VEC2 chirikov(VEC2 z, VEC2 c) {
-  z.y += c.y*sin(z.x);
-  z.x += c.x*z.y;
-  return z;
-}
-
-#if 1
-#define DO_LOOP(name) \
-  for (i = 0; i < iIters; ++i) { \
-    VEC2 ppz = pz; \
-    pz = z; \
-    z = name(z, c); \
-    if (dot(z, z) > ESCAPE) { break; } \
-    sumz.x += dot(z - pz, pz - ppz); \
-    sumz.y += dot(z - pz, z - pz); \
-    sumz.z += dot(z - ppz, z - ppz); \
-  }
-#else
-#define DO_LOOP(name) \
-  for (i = 0; i < iIters; ++i) { \
-    z = name(z, c); \
-    if (dot(z, z) > ESCAPE) { break; } \
-  }
-#endif
-
-vec3 fractal(VEC2 z, VEC2 c) {
-  VEC2 pz = z;
-  VEC3 sumz = VEC3(0.0, 0.0, 0.0);
-  int i;
-  switch (iType) {
-    case 0: DO_LOOP(mandelbrot); break;
-    case 1: DO_LOOP(burning_ship); break;
-    case 2: DO_LOOP(feather); break;
-    case 3: DO_LOOP(sfx); break;
-    case 4: DO_LOOP(henon); break;
-    case 5: DO_LOOP(duffing); break;
-    case 6: DO_LOOP(ikeda); break;
-    case 7: DO_LOOP(chirikov); break;
-  }
-
-  if (i != iIters) {
-    float n1 = sin(float(i) * 0.1) * 0.5 + 0.5;
-    float n2 = cos(float(i) * 0.1) * 0.5 + 0.5;
-    return vec3(n1, n2, 1.0) * (1.0 - float(FLAG_USE_COLOR)*0.85);
-  } else if (FLAG_USE_COLOR) {
-    sumz = abs(sumz) / iIters;
-    vec3 n1 = sin(abs(sumz * 5.0)) * 0.45 + 0.5;
-    return n1;
-  } else {
-    return vec3(0.0, 0.0, 0.0);
-  }
-}
-
-float rand(float s) {
-  return fract(sin(s*12.9898) * 43758.5453);
-}
-
-void main() {
-	//Get normalized screen coordinate
-	vec2 screen_pos = gl_FragCoord.xy - (iResolution.xy * 0.5);
-
-  vec3 col = vec3(0.0, 0.0, 0.0);
-  for (int i = 0; i < AA_LEVEL; ++i) {
-    vec2 dxy = vec2(rand(i*0.54321 + iTime), rand(i*0.12345 + iTime));
-    VEC2 c = VEC2((screen_pos + dxy) * vec2(1.0, -1.0) / iZoom - iCam);
-
-    if (FLAG_DRAW_MSET) {
-      col += fractal(c, c);
-    }
-    if (FLAG_DRAW_JSET) {
-      col += fractal(c, iJulia);
-    }
-  }
-
-  col /= AA_LEVEL;
-  if (FLAG_DRAW_MSET && FLAG_DRAW_JSET) {
-    col *= 0.5;
-  }
-  gl_FragColor = vec4(clamp(col, 0.0, 1.0), 1.0 / (iTime + 1.0));
-}
+<NotepadPlus>
+    <Session activeView="0">
+        <mainView activeIndex="3">
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="960" startPos="1187" endPos="1187" selMode="0" offset="0" wrapCount="1" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Program Files (x86)\Notepad++\change.log" backupFilePath="C:\Users\ethan\AppData\Roaming\Notepad++\backup\change.log@2021-09-30_112446" originalFileLastModifTimestamp="-837341696" originalFileLastModifTimestampHigh="30820775" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="7864439" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="63" xOffset="0" scrollWidth="568" startPos="4026" endPos="4026" selMode="0" offset="0" wrapCount="1" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\new-function\.gitattributes" backupFilePath="C:\Users\ethan\AppData\Roaming\Notepad++\backup\.gitattributes@2021-09-30_112535" originalFileLastModifTimestamp="973362220" originalFileLastModifTimestampHigh="30914012" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="15" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="48" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\.gitignore" backupFilePath="" originalFileLastModifTimestamp="577877340" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="1936879984" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="57" xOffset="0" scrollWidth="616" startPos="2174" endPos="2282" selMode="0" offset="0" wrapCount="1" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\frag.glsl" backupFilePath="C:\Users\ethan\AppData\Roaming\Notepad++\backup\frag.glsl@2021-09-30_113551" originalFileLastModifTimestamp="577967361" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="4259930" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="1" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="0" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\LICENSE" backupFilePath="" originalFileLastModifTimestamp="577887337" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="1927614127" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="1" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="0" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\LICENSE-FONT.txt" backupFilePath="" originalFileLastModifTimestamp="577897335" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="15628520" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="896" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="XML" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\MandelbrotMusic.vcxproj" backupFilePath="" originalFileLastModifTimestamp="577907346" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="7365971" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="936" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="XML" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\MandelbrotMusic.vcxproj.filters" backupFilePath="" originalFileLastModifTimestamp="577917343" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="712" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="XML" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\MandelbrotMusic.vcxproj.user" backupFilePath="" originalFileLastModifTimestamp="577927347" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="1" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="0" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\RobotoMono-Medium.ttf" backupFilePath="" originalFileLastModifTimestamp="577947336" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="472" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="C++" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\Main.cpp" backupFilePath="" originalFileLastModifTimestamp="577907346" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="1916210941" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="2440" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="Markdown (Default)" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\README.md" backupFilePath="" originalFileLastModifTimestamp="577927347" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="95" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="520" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="Normal Text" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\vert.glsl" backupFilePath="" originalFileLastModifTimestamp="577977353" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="7471201" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="776" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="1" lang="C++" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\WinAudio.cpp" backupFilePath="" originalFileLastModifTimestamp="577957364" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="0" mapWrapIndentMode="-1" mapIsWrap="no" />
+            <File firstVisibleLine="0" xOffset="0" scrollWidth="1" startPos="0" endPos="0" selMode="0" offset="0" wrapCount="0" lang="C++" encoding="-1" userReadOnly="no" filename="C:\Users\ethan\Documents\GitHub\FractalSoundExplorer\WinAudio.h" backupFilePath="" originalFileLastModifTimestamp="577957364" originalFileLastModifTimestampHigh="30914014" mapFirstVisibleDisplayLine="-1" mapFirstVisibleDocLine="-1" mapLastVisibleDocLine="-1" mapNbLine="-1" mapHigherPos="-1" mapWidth="-1" mapHeight="-1" mapKByteInDoc="16" mapWrapIndentMode="-1" mapIsWrap="no" />
+        </mainView>
+        <subView activeIndex="0" />
+    </Session>
+</NotepadPlus>
